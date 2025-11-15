161aeadba56b1afda9fb93ac328b41d79cc3d3e3
ABUBAKARKHAN-Stack <abubakar123456654123@gmail.com>
2025-11-15 22:00:21 +0500

    feat: Refactor Chai models and admin interface, add ChaiReview, Store, and ChaiCertificate models

--- chaiaurDjango/chai/admin.py
+++ chaiaurDjango/chai/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
-from .models import ChaiVerity
+from .models import ChaiVariety, ChaiReview, Store, ChaiCertificate
 
 # Register your models here.
 
-admin.site.register(ChaiVerity)
+
+class ChaiReviewInline(admin.TabularInline):
+    model = ChaiReview
+    extra = 2
+
+class ChaiVarietyAdmin(admin.ModelAdmin):
+    list_display = ("name", "chai_type", "date_added")
+    inlines = [ChaiReviewInline]
+
+
+class ChaiStoreAdmin(admin.ModelAdmin):
+    list_display = ("name", "location")
+    filter_horizontal = ("chai_varities",)
+
+
+class ChaiCertificateAdmin(admin.ModelAdmin):
+    list_display = ("chai", "certificate_number")
+
+
+admin.site.register(ChaiVariety,ChaiVarietyAdmin)
+admin.site.register(Store,ChaiStoreAdmin)
+admin.site.register(ChaiCertificate,ChaiCertificateAdmin)
